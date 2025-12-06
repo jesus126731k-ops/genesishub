c89ba30b2427b99859e3f641dc08712aea545716
Genesis Hub <jesus126731k@gmail.com>
2025-12-05 21:03:35 -0600

    Actualizar server.py y mantener todo limpio

--- server.py
+++ server.py
@@ -66,4 +66,5 @@ def x(s):
         return jsonify(json.load(f))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
